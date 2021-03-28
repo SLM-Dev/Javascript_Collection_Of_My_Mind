1c4833cdacb639fbdffeca31d7735d29236b15d4
Spencer Lee Meredith <16077691+SLM-Dev@users.noreply.github.com>
2021-03-28 03:44:53 +0000

    ✏️ fix: correct minor typos in comments

--- console.log/Learn_Console.js
+++ console.log/Learn_Console.js
@@ -1,9 +1,9 @@
 // | = output
 // Print To Screen
-// Instructions type console dot log then a parenthes () open and closing inside of parenthese () what ever you put will be print out to the screen. Make sure to put quotes '' inside the parenthses (). If you are putting text example below.
+// Instructions type console dot log then a parentheses () open and closing inside of parentheses () what ever you put will be print out to the screen. Make sure to put quotes '' inside the parenthses (). If you are putting text example below.
 
 
-// To use comment or uncomment the console you would like to use. Then type in the termail the name of the file you would like output. i.e node Learn_Console.js
+// To use comment or uncomment the console you would like to use. Then type in the terminal  the name of the file you would like output. e.g  node Learn_Console.js
 
 
 
